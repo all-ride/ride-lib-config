6992b4f23dbf63104f29a0f44d260ff5db9191e8
Kristof Serré <kristofserre@gmail.com>
2014-03-28 13:27:49 +0100

    updated API documentation

--- src/ride/library/config/parser/IniParser.php
+++ src/ride/library/config/parser/IniParser.php
@@ -79,7 +79,7 @@ class IniParser implements Parser {
      * as a key or a value
      * @param string $key Key for the provided values (for recursive calls)
      * @return string Ini of the provided config
-     * @throws ride\library\config\exception\ConfigException when the provided
+     * @throws \ride\library\config\exception\ConfigException when the provided
      * config is not an array and no key is provided
      */
     protected function parseToIni($var, $key = null) {
